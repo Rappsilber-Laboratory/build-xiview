6e496b36318bb7f355d96520029cc17765caf844
colin-combe <colin.combe@ed.ac.uk>
2021-11-01 15:57:06 +0000

    updating

--- xiSpecConfig.php
+++ xiSpecConfig.php
@@ -1,4 +1,4 @@
 <?php
-echo "CLMSUI.xiSpecBaseDir = \"/spectrum/\";\n"; // you may need to edit this depending on where xiView_container is in web server folder
-echo "CLMSUI.xiAnnotRoot = \"https://xiview.org/xiAnnotator/\";\n";
+echo "window.xiSpecBaseDir = \"/spectrum/\";\n"; // you may need to edit this depending on where xiView_container is in web server folder
+echo "window.xiAnnotRoot = \"http://localhost/xiAnnotator/\";\n";;
 ?>
