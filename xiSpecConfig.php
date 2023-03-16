846bd17fbd3cb90f1eeb41ea2843be9a48661898
root <root@rappsilberlab.org>
2023-03-16 15:04:07 +0100

    whats actually on server

--- xiSpecConfig.php
+++ xiSpecConfig.php
@@ -1,4 +1,4 @@
 <?php
 echo "window.xiSpecBaseDir = \"/xiUI_lab/spectrum/\";\n"; // you may need to edit this depending on where xiView_container is in web server folder
-echo "window.xiAnnotRoot = \"http://localhost/xiAnnotator/\";\n";
+echo "window.xiAnnotRoot = \"/xiAnnotator/\";\n";
 ?>
