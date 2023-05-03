e9fdd917844b23019fa77f10b15600ec093033f4
colin-combe <colin.combe@ed.ac.uk>
2023-05-03 18:06:06 +0100

    aggregation fixes; also update to webpack 5

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,6 @@
 require("webpack");
 const path = require("path");
-const merge = require("webpack-merge");
+const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
