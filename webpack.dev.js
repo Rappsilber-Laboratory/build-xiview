a89e00a4cd4fef6bc281b977f3e9d8b924507cc5
colin-combe <colin.combe@ed.ac.uk>
2023-05-04 10:48:11 +0100

    fixes

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,19 +5,5 @@ const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
     mode: "development",
-    devtool: "source-map",
-    module: {
-        rules: [
-            // {
-            //     test: /\.(js)$/,
-            //     use: 'babel-loader',
-            //     exclude: /node_modules/
-            // }
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname),
-        compress: true,
-        port: 9000
-    }
+    devtool: 'inline-source-map'
 });
--- xiSpecConfig.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "window.xiSpecBaseDir = \"/xiUI_lab/spectrum/\";\n"; // you may need to edit this depending on where xiView_container is in web server folder
-echo "window.xiAnnotRoot = \"http://localhost/xiAnnotator/\";\n";
-?>
