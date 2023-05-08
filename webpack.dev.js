bd76a76ee5cb5d9e0d586e792efe19224ef80aab
colin-combe <colin.combe@ed.ac.uk>
2023-05-08 06:03:54 +0100

    multi server version xiview, fix splitter

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,23 +1,9 @@
 require("webpack");
 const path = require("path");
-const merge = require("webpack-merge");
+const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
     mode: "development",
-    devtool: "source-map",
-    module: {
-        rules: [
-            // {
-            //     test: /\.(js)$/,
-            //     use: 'babel-loader',
-            //     exclude: /node_modules/
-            // }
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname),
-        compress: true,
-        port: 9000
-    }
+    devtool: 'inline-source-map'
 });
