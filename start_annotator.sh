ee7d2f71f2a81cd9cc9ec534e038232cb4d133eb
colin combe <colin.combe@ed.ac.uk>
2023-10-16 14:06:24 +0100

    remove stuff that isn't xiview JS

--- start_annotator.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-touch /home/col/annotator/command_has_run;
-while true; do sleep 1; /usr/lib/jvm/java-1.8.0-openjdk-amd64/bin/java -DBASE_URI=https://0.0.0.0:8083/xiAnnotator/ -jar ./xiAnnotator-1.4.32-jar-with-dependencies.jar; done;
